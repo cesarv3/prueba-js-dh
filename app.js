f2809e6612ad934bbae54649366464e4d10dd194
cesar villa <cvilla@certuit.com>
2021-05-17 19:31:26 -0700

    Me falto el filter no supe como pasar el parametro

--- app.js
+++ app.js
@@ -6,22 +6,48 @@
            * pendiente
          )
 */
+const fs = require('fs');
 
 exports.leerTareas = function () {
-    /*
-        En esta función debes leer y retornar las tareas registradas.
-    */
+    const listado = fs.readFileSync('./tareas.json', 'utf-8');
+    let arreglo = [];
+    arreglo = JSON.parse(listado);
+    console.log(typeof listado);
+    return JSON.parse(listado);
 }
 
 exports.agregarTarea = function (tarea) {
     /*
         Registra y guarda una nueva tarea.
     */
+   let listado = fs.readFileSync('./tareas.json', 'utf-8');
+    let listadoArray = [];
+   console.log( JSON.parse(listado));
+   listadoArray = JSON.parse(listado);
+   console.log(typeof listadoArray);
+   listadoArray.push(tarea);
+   const tareasMod = JSON.stringify(listadoArray);
+   fs.writeFileSync('./tareas.json', tareasMod, (err) => {
+       if(err){
+           throw err;
+       } console.log('Guardado');
+   })
+   
 }
 
 exports.filtrarTareasPorEstado = function (estado) {
     /*
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
-    */
+    */ //let propiedad = estado;
+    let propiedad = estado;
+        let listado = fs.readFileSync('./tareas.json', 'utf-8');
+        let listadoArray = [];
+        listadoArray = JSON.parse(listado);
+        console.log('Filter' + listadoArray);
+        let nuevo = listadoArray.filter((estado) => {
+            return listadoArray.estado === propiedad;
+        });
+        return nuevo;
+
 }
